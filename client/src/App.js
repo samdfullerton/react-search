40991e8376d6a95d79e4376fcf9ec87a026d5ec1
Sam Fullerton <Samdfullerton@gmail.com>
2020-11-29 17:44:27 -0600

    setup pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import homepage from "./pages/homepage";
+// books homepage
+import Detail from "./pages/Detail";
+// details is saved page
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
     <div className="App">
-     
+
     </div>
   );
 }
